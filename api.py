0b00a38a75ca4757ebc1b6f82ea6a0b2fbf76960
Bagus Anugrah <bagus.anugrah71@gmail.com>
2024-12-31 00:22:23 +0700

    mengganti argumen pada api.py karena pemborosan

--- api.py
+++ api.py
@@ -39,7 +39,7 @@ async def process_api(data: InputData):
 
         list_dokumen = TemuBalik.bacaIsiDokumen(directory_path)
 
-        hasil_vsm = TemuBalik.temuBalikVSM(list_dokumen, TemuBalik.prosesQuery(query.lower()))
+        hasil_vsm = TemuBalik.temuBalikVSM(list_dokumen, query_info)
 
         result = {
             "directory_path": directory_path,
